054d032ba9d19a1341adc18e5a575dc67c6da1ec
ivan <idimitryz@gmail.com>
2022-07-25 00:36:27 +0200

    feat: add getRandomThing

--- src/populate-db/thing-description-loader.js
+++ src/populate-db/thing-description-loader.js
@@ -5,6 +5,9 @@ const readFile = util.promisify(fs.readFile);
 const readDirectory = util.promisify(fs.readdir);
 require('dotenv').config();
 
+const { Random } = require('../common/random');
+const config = require('../config');
+
 class ThingDescriptionLoader {
   constructor(path = process.env.THING_DESCRIPTION_PATH) {
     this.path = path;
@@ -26,7 +29,10 @@ class ThingDescriptionLoader {
       const directoryPath = path.join(__dirname, `${this.path}/${directory}`);
       const filenamesArray = await readDirectory(directoryPath);
       for (const filename of filenamesArray) {
-        const thingDescription = await this.readThingDirectoryFile(directoryPath, filename);
+        const thingDescription = await this.readThingDirectoryFile(
+          directoryPath,
+          filename,
+        );
         //console.log(thingDescription);
         this.thingDescriptions[directory].push(thingDescription);
       }
@@ -45,6 +51,20 @@ class ThingDescriptionLoader {
       console.error(`*** Error reading file ${filename} from ${directoryPath} ***`);
       console.error(error);
     }
+  }
+
+  #selector() {
+    const random = Random.getRandom();
+    const distribution = config.thingDescriptionDistribution;
+    if (random < distribution.simple) return 'simple';
+    else if (random < distribution.medium) return 'medium';
+    return 'complex';
+  }
+
+  getRandomThing() {
+    const category = this.#selector();
+    const index = Random.getRandomInt(0, this.thingDescriptions[category].length);
+    return this.thingDescriptions[category][index];
   }
 }
 

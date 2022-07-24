d1f8fc8293b47a82b8a40299d93639fedf887b26
ivan <idimitryz@gmail.com>
2022-07-25 00:37:18 +0200

    feat: on execution, populates TDs

--- src/populate-db/index.js
+++ src/populate-db/index.js
@@ -1,9 +1,31 @@
+const { Random } = require('../common/random');
 const { ThingDescriptionLoader } = require('./thing-description-loader');
+const { ThingDirectoryClient } = require('./thing-directory-client');
+
+Random.setSeed(process.env.SEED);
+
 const thingDescriptionLoader = new ThingDescriptionLoader();
+const thingDirectoryClient = new ThingDirectoryClient();
+
+const reset = async () => {
+  try {
+    const response = await thingDirectoryClient.getAll();
+    if (response.data.length === 0) throw Error('TDD is not empty');
+  } catch (error) {
+    console.log(error);
+    throw Error(`Problem reseting TDD`);
+  }
+};
 
 const start = async () => {
+  await reset();
   await thingDescriptionLoader.load();
-  console.log(thingDescriptionLoader.thingDescriptions);
+  const numberofThingDescriptions = process.env.NUMBER_OF_THING_DESCRIPTIONS;
+  for (let i = 0; i <= numberofThingDescriptions; ) {
+    const randomThing = thingDescriptionLoader.getRandomThing();
+    const createThing = await thingDirectoryClient.createThing(randomThing);
+    if (createThing) i++;
+  }
 };
 
 start().then().catch(console.error);

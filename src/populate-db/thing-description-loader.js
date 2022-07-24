572a23c7ec720b158adb37b96db87564ae068772
ivan <idimitryz@gmail.com>
2022-07-24 19:11:33 +0200

    feat: load tds from examplex-td

---
+++ src/populate-db/thing-description-loader.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const readFile = util.promisify(fs.readFile);
+const readDirectory = util.promisify(fs.readdir);
+require('dotenv').config();
+
+class ThingDescriptionLoader {
+  constructor(path = process.env.THING_DESCRIPTION_PATH) {
+    this.path = path;
+    this.thingDescriptions = {
+      simple: [],
+      medium: [],
+      complex: [],
+    };
+  }
+
+  async load() {
+    for (const tdCategory in this.thingDescriptions)
+      await this.readThingDirectoryFolder(tdCategory);
+    return this.thingDescription;
+  }
+
+  async readThingDirectoryFolder(directory) {
+    try {
+      const directoryPath = path.join(__dirname, `${this.path}/${directory}`);
+      const filenamesArray = await readDirectory(directoryPath);
+      for (const filename of filenamesArray) {
+        const thingDescription = await this.readThingDirectoryFile(directoryPath, filename);
+        //console.log(thingDescription);
+        this.thingDescriptions[directory].push(thingDescription);
+      }
+    } catch (error) {
+      console.error(`*** Error reading directory ${directory} ***`);
+      console.log(error);
+    }
+  }
+
+  async readThingDirectoryFile(directoryPath, filename) {
+    try {
+      const filePath = path.join(directoryPath, filename);
+      const fileData = await readFile(filePath, 'utf8');
+      return JSON.parse(fileData);
+    } catch (error) {
+      console.error(`*** Error reading file ${filename} from ${directoryPath} ***`);
+      console.error(error);
+    }
+  }
+}
+
+module.exports = {
+  ThingDescriptionLoader,
+};
